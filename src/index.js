c4c20294657bc8c9c844cfe096683f5c3a877f5c
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2024-10-06 17:45:40 +0100

    add: homepage content

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 
-
+import "./styles.css"; 
 import {greeting} from "./greeting.js";
-import "./styles.css";
+
 
 
  console.log(greeting);
  alert("food is Ready\n Welcome!");
+ console.log("break the break up");
 
  
 
